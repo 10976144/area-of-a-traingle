cc98d9047d2f92432d5b8fb0e4260f2d09deb25b
10976144 <102815438+10976144@users.noreply.github.com>
2022-04-07 17:27:31 +0000

    Create assignment.cpp

---
+++ assignment.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int base;
+    int height;
+    double area;
+    
+    cout << "Input your base and press enter" << endl;
+    cin >> base;
+    
+    cout << "Input your height and press enter" << endl;
+    cin >> height;
+    
+    area = 0.5 * base * height;
+    
+    cout << "The area of the triangle is: " << area << endl;
+    
+
+    return 0;
+}
